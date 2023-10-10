4484a8e6bc4b325f5a3ffd8bc30e42bdbfe7713d
KARTHIK <114098552+KARTHIZz@users.noreply.github.com>
2023-10-10 18:56:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "php-app.chcjpppgghru.ap-northeast-1.rds.amazonaws.com";
+$servername = "phpapp.chcjpppgghru.ap-northeast-1.rds.amazonaws.com";
 $username = "karthik";
 $password = "karthik123";
 $dbname = "karthik";
